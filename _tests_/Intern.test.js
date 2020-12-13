863d9c3ac6328b0c77387dd36b8d9df9fe3669b5
Andrew Ahler <andrewahler@gmail.com>
2020-12-13 14:47:36 -0600

    added intern test

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,22 +1,22 @@
 const Intern = require('../lib/Intern.js')
 
 test('creates an engineer object', () => {
-    const intern = new Intern ('tom ', '2222', 'cpu@cpu.com', 'aa')
+    const intern = new Intern ('tom ', '2222', 'cpu@cpu.com', 'vandy')
     
     expect(intern.name).toBe('tom ');
     expect(intern.id).toBe('2222');
     expect(intern.email).toBe('cpu@cpu.com');
-    expect(intern.github).toBe('aa');
+    expect(intern.school).toBe('vandy');
 });
 
-test ("intern's github username", () => {
-    const intern = new Intern ('tom ', '2222', 'cpu@cpu.com', 'aa')
+test ("intern's school username", () => {
+    const intern = new Intern ('tom ', '2222', 'cpu@cpu.com', 'vandy')
 
     expect(intern.getGithub()).toEqual(expect.stringContaining('cpu@cpu.com'));
 });
 
 test("intern's role", () => {
-    const intern = new Intern ('tom ', '2222','cpu@cpu.com', 'aa')
+    const intern = new Intern ('tom ', '2222','cpu@cpu.com', 'vandy')
 
     expect(intern.getRole()).toEqual(expect.stringContaining('Intern'));
 });
