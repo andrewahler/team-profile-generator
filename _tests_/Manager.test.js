233efe3dec9cbd606130bfecc3c4f8797a0eade5
Andrew Ahler <andrewahler@gmail.com>
2020-12-13 14:53:18 -0600

    added manager test

---
+++ _tests_/Manager.test.js
@@ -0,0 +1,22 @@
+const Manager = require('../lib/Manager.js')
+
+test('creates an manager object', () => {
+    const manager = new Manager ('tom ', '2222', 'cpu@cpu.com', '2356')
+    
+    expect(manager.name).toBe('tom ');
+    expect(manager.id).toBe('2222');
+    expect(manager.email).toBe('cpu@cpu.com');
+    expect(manager.officeNumber).toBe('2356');
+});
+
+test ("manager's officeNumber username", () => {
+    const manager = new Manager ('tom ', '2222', 'cpu@cpu.com', '2356')
+
+    expect(manager.getGithub()).toEqual(expect.stringContaining('cpu@cpu.com'));
+});
+
+test("manager's role", () => {
+    const manager = new Manager ('tom ', '2222','cpu@cpu.com', '2356')
+
+    expect(manager.getRole()).toEqual(expect.stringContaining('Manager'));
+});
