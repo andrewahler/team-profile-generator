f367a01e874f1367651104694748905b748780ff
Andrew Ahler <andrewahler@gmail.com>
2020-12-13 15:58:09 -0600

    added employee test

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,1 +1,27 @@
-test
+const Employee = require('../lib/Employee.js');
+
+test('employee object', () => {
+    const employee = new Employee('tom', '2356', 'me@me.com');
+
+    expect(employee.name).toBe('tom');
+    expect(employee.id).toBe('2356');
+    expect(employee.email).toBe('me@me.com');
+});
+
+test("employee name", () => {
+    const employee = new Employee('tom', '2356', 'me@me.com');
+
+    expect(employee.getName()).toEqual(expect.stringContaining('tom'));
+});
+
+test("employee ID", () => {
+    const employee = new Employee('tom', '2356', 'me@me.com');
+
+    expect(employee.getId()).toEqual(expect.stringContaining('2356'));
+});
+
+test("employee email", () => {
+    const employee = new Employee('tom', '2356', 'me@me.com');
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining('me@me.com'));
+});
